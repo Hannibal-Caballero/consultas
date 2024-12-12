bbe4e924fea94ac02ddd43b9121e6c57ac578c36
Hannibal-Caballero <anibalcaballero2206@gmail.com>
2024-12-12 06:59:54 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-// Aquí puedes añadir cualquier script que necesites para tu página web
-// Por ejemplo, si quieres añadir interactividad a los elementos de la página
+
 
 document.addEventListener('DOMContentLoaded', function() {
-    // Ejemplo de código JavaScript
+
     console.log('La página se ha cargado correctamente');
 });
-ç
+
