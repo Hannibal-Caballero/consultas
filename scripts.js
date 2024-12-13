cb2b8b20790fe73dae69f0f92a1b54e3cfcafd9b
Hannibal-Caballero <anibalcaballero2206@gmail.com>
2024-12-13 19:21:50 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,7 @@
-// Aquí puedes añadir cualquier script que necesites para tu página web
-// Por ejemplo, si quieres añadir interactividad a los elementos de la página
+const tarjetas = document.querySelectorAll('.tarjeta');
 
-document.addEventListener('DOMContentLoaded', function() {
-    // Ejemplo de código JavaScript
-    console.log('La página se ha cargado correctamente');
-});
-ç
+const header = document.querySelectorAll(".header"); 
+
+const inicioBtn = document.getElementById("nav1");
+
+const footer = document.getElementsByClassName("footer");
